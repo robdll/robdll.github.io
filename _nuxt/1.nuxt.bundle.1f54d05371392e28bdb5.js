d30b864fad9fd580588874f3b129d7abfba5af89
koop4 <dilillo.roberto@gmail.com>
2017-04-13 21:50:27 -0500

    add dist folder to project

---
+++ _nuxt/1.nuxt.bundle.1f54d05371392e28bdb5.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{40:function(n,e,t){var r=t(1)(null,t(43),null,null);n.exports=r.exports},43:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("nuxt")],1)},staticRenderFns:[]}}});
