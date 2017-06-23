f46a4a1f9584f2e65b9dec85fe8544fa6e8c9483
koop4 <dilillo.roberto@gmail.com>
2017-06-23 12:29:01 +0200

    fakewriter now act on innerhtml

--- js/fakeWriter.js
+++ js/fakeWriter.js
@@ -1,36 +1,46 @@
 document.addEventListener("DOMContentLoaded", function(){
-  var professions = [ "front-end", "back-end", "web", "full-stack", "game", "software", "NERD!" ];
-  var spanContainer = document.getElementById('word');
-  var professionIndex = 0;
+  var professions = [ "back-end developer ", 
+    "front-end developer ", 
+    "game developer ", 
+    "software engineer ", 
+    "nerd" 
+  ];
   
-  addProfession();
+  var whoAmIparagraph = document.getElementById('whoAmI');
+  var professionIndex  = 0;
+
+    addProfession() 
+
+
+  setInterval(function(){
+    professionIndex = -1;
+    deleteProfession();
+  },25000) 
   
   function addProfession(){
     var word = ' '+professions[professionIndex];
-    if(professionIndex !== professions.length-1){
-      word+= ' developer ';
-    }
     var index = 0;
     var adder = setInterval(function(){ 
-      var span = document.createElement("span");
-      span.innerHTML = word[index];
-      spanContainer.appendChild(span)
+      whoAmIparagraph.innerHTML = whoAmIparagraph.innerHTML + word[index];
       index++;
       if(index === word.length) { 
         clearInterval(adder);
-        if(professionIndex !== professions.length-1){
-          removeSpans();  
+        if(professionIndex !== professions.length-1) {
+          deleteProfession();  
+        }
+        else {
         }
       }
     }, 150);
   }
   
-  function removeSpans(){  
-    var remover = setInterval(function(){ 
-      spanContainer.removeChild(spanContainer.lastChild);
-      if(!spanContainer.children.length) { 
-        clearInterval(remover);
+
+  function deleteProfession() {  
+    var backSpacer = setInterval(function(){ 
+      whoAmIparagraph.innerHTML = whoAmIparagraph.innerHTML.substring(0, whoAmIparagraph.innerHTML.length-1);
+      if(!whoAmIparagraph.innerHTML.length) {         
         professionIndex++;
+        clearInterval(backSpacer);
         addProfession();
       }
     }, 80);
