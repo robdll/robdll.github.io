f76a98a9141f58bbe35bb9978220d634969bd93e
koop4 <dilillo.roberto@gmail.com>
2017-07-23 01:31:23 +0200

    improve navigation

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -3,3 +3,10 @@ function smoothScroll(id) {
     scrollTop: $(id).offset().top
   }, 1000);
 }
+
+var nextSection = '';
+function navigatorSwitcher() {
+  nextSection = nextSection === '#portfolioanchor' ? '#me' : '#portfolioanchor';
+  smoothScroll(nextSection);
+}
+
