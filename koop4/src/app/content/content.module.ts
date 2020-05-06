e1bd8ef7c62a9673d06d153ac74917181b091fa4
koop4 <dilillo.roberto@gmail.com>
2020-05-06 18:47:23 +0200

    adds section me

--- koop4/src/app/content/content.module.ts
+++ koop4/src/app/content/content.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentComponent } from './content.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [ContentComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     ContentComponent
