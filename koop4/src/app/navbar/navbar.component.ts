81af60b07315c1f1a5c28f2acff0491f35693084
koop4 <dilillo.roberto@gmail.com>
2020-05-06 12:53:12 +0200

    edit nav font color

--- koop4/src/app/navbar/navbar.component.ts
+++ koop4/src/app/navbar/navbar.component.ts
@@ -4,7 +4,7 @@ import { faLanguage } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent{
 
