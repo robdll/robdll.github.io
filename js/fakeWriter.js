542e41535a481f9ae22a3ffb1777edc4f79ec62c
robdll <dilillo.roberto@gmail.com>
2022-07-22 19:34:21 -0300

    seo update and style improvements

--- js/fakeWriter.js
+++ js/fakeWriter.js
@@ -1,46 +1,50 @@
-document.addEventListener("DOMContentLoaded", function(){
-  var professions = [ 
-    "Frontend developer?", 
-    "Backend developer?", 
-    "Fullstack developer?", 
-    "Game developer?", 
-    "Software engineer?"
+document.addEventListener("DOMContentLoaded", function () {
+  var professions = [
+    "Code mentor?",
+    "Frontend developer?",
+    "Backend developer?",
+    "Game developer?",
+    "Software engineer?",
+    "Devops engineer?",
+    "Fullstack developer?",
   ];
-  var whoAmI = document.getElementById('heroWriter');
-  var professionIdx  = 0;
-  addProfession() 
-  
-  function addProfession(){
+  var whoAmI = document.getElementById("heroWriter");
+  var professionIdx = 0;
+  waitAMoment();
+
+  function addProfession() {
     let profession = professions[professionIdx];
     let charIdx = 0;
-    let writer = setInterval(function(){ 
+    let writer = setInterval(function () {
       whoAmI.innerHTML = whoAmI.innerHTML + profession[charIdx];
       charIdx++;
       let professionWritten = charIdx === profession.length;
-      if(professionWritten) { 
+      if (professionWritten) {
         clearInterval(writer);
-        waitAMoment();  
+        waitAMoment();
       }
     }, 120);
   }
-  
+
   function waitAMoment() {
     let waitALittle = setInterval(() => {
-      clearTimeout(waitALittle)
+      clearTimeout(waitALittle);
       deleteProfession();
     }, 800);
   }
 
-  function deleteProfession() { 
-    let deleter = setInterval(function(){  
-      whoAmI.innerHTML = whoAmI.innerHTML.substring(0, whoAmI.innerHTML.length-1);
+  function deleteProfession() {
+    let deleter = setInterval(function () {
+      whoAmI.innerHTML = whoAmI.innerHTML.substring(
+        0,
+        whoAmI.innerHTML.length - 1
+      );
       let professionDeleted = !whoAmI.innerHTML.length;
-      if(professionDeleted) {         
-        professionIdx = professionIdx === 4 ? 1 : professionIdx+1;
+      if (professionDeleted) {
+        professionIdx = professionIdx === 4 ? 1 : professionIdx + 1;
         clearInterval(deleter);
         addProfession();
       }
     }, 60);
   }
-
 });
