0fa5abe934e386836ad2142f8114b57c69114172
Roberto Di Lillo <roberto.dilillo@garycom.com>
2024-11-01 10:05:43 +0100

    update footer

---
+++ components/footer.js
@@ -0,0 +1,19 @@
+document.getElementById("footer-container").innerHTML = `
+  <div class="contacts">
+    <p>
+      Dont'be a stranger. 
+    </p>
+    <div class="contact-grid">
+      <span>Write me an </span>
+      <a id="email" href="mailto:dilillo.roberto@gmail.com">Email</a> </li>
+      <span>Follow me on </span>
+      <a id="github" href="https://github.com/robdll" target="_blank">GitHub</a></li>
+      <span>Add me on </span>
+      <a id="linkedin" href="https://www.linkedin.com/in/roberto-di-lillo/" target="_blank">Linkedin</a></li>
+      <span>Visit me on </span>
+      <a id="stackoverflow" href="https://stackoverflow.com/users/2324133/robdll" target="_blank">StackOverflow</a></li>
+      <span>Checkout my </span>
+      <a id="resume" href="./resources/Rob_Resume_2024.pdf" download>Resume</a></li>
+    </div>
+  </div>
+`;
